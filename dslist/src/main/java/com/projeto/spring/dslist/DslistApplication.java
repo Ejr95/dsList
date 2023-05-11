5349fd48d91e08670e313822abedec5652b07d8e
Elizeu Magalhães <Elizeu.2230556@aluno.faculdadedescomplica.com.br>
2023-05-10 20:42:41 -0400

    projeto criado

--- dslist/src/main/java/com/projeto/spring/dslist/DslistApplication.java
+++ dslist/src/main/java/com/projeto/spring/dslist/DslistApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.projeto.spring.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DslistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DslistApplication.class, args);
 	}
 
 }
